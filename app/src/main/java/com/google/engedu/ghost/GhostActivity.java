04d2dbb4c243ae06cd60e6de1081a72c04e1e037
Jayadeep KM <kmjayadeep@gmail.com>
2016-04-10 10:55:17 +0530

    start

--- app/src/main/java/com/google/engedu/ghost/GhostActivity.java
+++ app/src/main/java/com/google/engedu/ghost/GhostActivity.java
@@ -3,11 +3,15 @@ package com.google.engedu.ghost;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 
 
@@ -17,58 +21,66 @@ public class GhostActivity extends AppCompatActivity {
     private GhostDictionary dictionary;
     private boolean userTurn = false;
     private Random random = new Random();
+    TextView tvStatus, tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ghost);
+        tvText = (TextView) findViewById(R.id.ghostText);
+        tvStatus = (TextView) findViewById(R.id.gameStatus);
+        findViewById(R.id.restart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onStart(null);
+            }
+        });
+        try {
+            InputStream is = getAssets().open("words.txt");
+            dictionary = new SimpleDictionary(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         onStart(null);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ghost, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void computerTurn() {
-        TextView label = (TextView) findViewById(R.id.gameStatus);
         // Do computer turn stuff then make it the user's turn again
         userTurn = true;
-        label.setText(USER_TURN);
+        tvStatus.setText(USER_TURN);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (Character.isLetter(event.getUnicodeChar())) {
+            Log.i("got char", keyCode + "");
+
+            String text = tvText.getText().toString();
+            text += (char) event.getUnicodeChar();
+            tvText.setText(text);
+
+
+        } else {
+            Log.i("got not char", keyCode + "");
+            return super.onKeyUp(keyCode, event);
+        }
+        return true;
     }
 
     /**
      * Handler for the "Reset" button.
      * Randomly determines whether the game starts with a user turn or a computer turn.
+     *
      * @param view
      * @return true
      */
     public boolean onStart(View view) {
         userTurn = random.nextBoolean();
-        TextView text = (TextView) findViewById(R.id.ghostText);
-        text.setText("");
-        TextView label = (TextView) findViewById(R.id.gameStatus);
+        tvText.setText("");
         if (userTurn) {
-            label.setText(USER_TURN);
+            tvStatus.setText(USER_TURN);
         } else {
-            label.setText(COMPUTER_TURN);
+            tvStatus.setText(COMPUTER_TURN);
             computerTurn();
         }
         return true;
